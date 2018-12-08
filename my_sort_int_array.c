dc2eec23db348bcafdbee1b77d098bd6427e4306
Léo Garret <35040105+leogarret@users.noreply.github.com>
2018-12-08 11:12:41 +0100

    Create my_sort_int_array.c

---
+++ my_sort_int_array.c
@@ -0,0 +1,15 @@
+void my_sort_int_array(int *array, int size)
+{
+    int i = 1;
+    int j;
+
+    while (i != size) {
+        if (array[i - 1] > array[i]) {
+            j = array[i - 1];
+            array[i - 1] = array[i];
+            array[i] = j;
+            i = 1;
+	      } else
+       	    i++;
+    }
+}
